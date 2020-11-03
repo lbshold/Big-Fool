b628627551894dbc6789a910c3d0642f73d9aab3
liusj <605715694>
2020-11-03 14:14:21 +0800

    添加swagger模块

---
+++ big-fool/big-fool-common/big-fool-common-swagger/src/main/java/top/lconcise/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package top.lconcise;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @author liusj
+ * @date 2020/11/3
+ * <p>
+ * SwaggerConfig 配置类.
+ * 禁用方法1：使用注解@Profile({"dev","test"})
+ * 表示在开发或测试环境开启，而在生产关闭。（推荐使用） 禁用方法2：使用注解@ConditionalOnProperty(name = "swagger.enable",
+ * havingValue = "true") 然后在测试配置或者开发配置中添加swagger.enable=true即可开启，生产环境不填则默认关闭Swagger.
+ */
+@Profile({"dev", "test"})
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Big-Fool Swagger api文档")
+                .description("")
+                .termsOfServiceUrl("https://www.jianshu.com/u/ce4cf486d279")
+                .version("1.0")
+                .build();
+    }
+}
