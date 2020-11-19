989de8aae59bbedaa8e8f8c80aaf69b4dd37d1e3
liusj <605715694>
2020-11-19 16:02:24 +0800

    feat(practice): ThreadLocal

--- big-fool/big-fool-practice/src/main/java/top/lconcise/practice/ThreadLocalDemo.java
+++ big-fool/big-fool-practice/src/main/java/top/lconcise/practice/ThreadLocalDemo.java
@@ -30,6 +30,15 @@ public class ThreadLocalDemo {
      *      设置当前线程的变量副本。
      *
      * 2.how
+     *   example 01
+     *   example 02
+     *
+     * 3.阿里Java 手册相关
+     *   a.SimpleDateFormat 线程不安全，建议使用通过ThreadLocal来使用
+     *   b.必须回收自定义的ThreadLocal变量，尤其在线程池场景下，线程经常会被复用，
+     *   如果不清理自定义的 ThreadLocal 变量，可能会影响后续业务逻辑和造成内存泄露等问题。
+     *   尽量在代理中使用 try-finally 块进行回收。
+     *
      */
 
     private static final AtomicInteger atomicInteger = new AtomicInteger();
--- big-fool/big-fool-practice/src/main/java/top/lconcise/practice/ThreadLocalDemo2.java
+++ big-fool/big-fool-practice/src/main/java/top/lconcise/practice/ThreadLocalDemo2.java
@@ -3,6 +3,8 @@ package top.lconcise.practice;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author liusj
@@ -10,6 +12,14 @@ import java.util.Date;
  */
 public class ThreadLocalDemo2 {
 
+    /**
+     * 为什么要这么做呢？
+     *  SimpleDateFormat 是线程不安全的，使用ThreadLocal 保证了线程安全。
+     *  ThreadLocal对象使用static修饰，ThreadLocal无法解决共享对象的更新问题。
+     *      说明：这个变量是针对一个线程内所有操作共享的，所以设置为静态变量，所有此类实例共享此静态变量，
+     *  也就是说在类第一次被使用时装载，只分配一块存储空间，所有此类的对象(只要是这个线程内定义的)都可
+     *  以操控这个变量。
+     */
     private static final ThreadLocal<DateFormat> df = new ThreadLocal<DateFormat>() {
         @Override
         protected DateFormat initialValue() {
@@ -22,5 +32,10 @@ public class ThreadLocalDemo2 {
         System.out.println(df.get().format(new Date()));
         System.out.println(df.get().format(new Date()));
         System.out.println(df.get().format(new Date()));
+
+
+        // Random  ThreadLocalRandom
+        System.out.println(new Random().nextInt(10));
+        System.out.println(ThreadLocalRandom.current().nextInt(10));
     }
 }
