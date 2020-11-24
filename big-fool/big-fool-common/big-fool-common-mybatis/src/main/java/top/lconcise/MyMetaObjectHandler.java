e71d0148c7f8015f5ff4e3b787bc594786ac291e
liusj <605715694>
2020-11-24 11:04:38 +0800

    feat(mybatis): 自动填充create_time，update_time

---
+++ big-fool/big-fool-common/big-fool-common-mybatis/src/main/java/top/lconcise/MyMetaObjectHandler.java
@@ -0,0 +1,30 @@
+package top.lconcise;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * 自动填充.
+ *
+ * @author liusj
+ * @date 2020/11/24
+ */
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "create_time", () -> LocalDateTime.now(), LocalDateTime.class);
+        this.strictUpdateFill(metaObject, "update_time", () -> LocalDateTime.now(), LocalDateTime.class);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "update_time", () -> LocalDateTime.now(), LocalDateTime.class);
+    }
+}
