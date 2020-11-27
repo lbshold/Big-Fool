e56d4b1dfaae0f2b245351fa85274d10669cfb3f
liusj <605715694>
2020-11-27 11:01:56 +0800

    feat(template): Volatile

---
+++ big-fool/big-fool-practice/src/main/java/top/lconcise/practice/VolatileDemo02.java
@@ -0,0 +1,53 @@
+package top.lconcise.practice;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+/**
+ * @author liusj
+ * @date 2020/11/26
+ */
+public class VolatileDemo02 {
+
+    /**
+     * 对基本类型的赋值和修改是原子性的，而对共享变量的修改并不是原子性
+     */
+
+    private volatile static int value = 0;
+    private static AtomicInteger atomicInteger = new AtomicInteger();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread01 = new Thread(() -> {
+            IntStream.range(0, 500).forEach(i -> value++);
+        });
+        Thread thread02 = new Thread(() -> {
+            IntStream.range(0, 500).forEach(i -> value++);
+        });
+
+        thread01.start();
+        thread02.start();
+        thread01.join();
+        thread02.join();
+
+        System.out.println(value);
+
+        Thread thread03 = new Thread(() -> {
+            IntStream.range(0, 500).forEach(i -> {
+                atomicInteger.getAndIncrement();
+            });
+        });
+
+        Thread thread04 = new Thread(() -> {
+            IntStream.range(0, 500).forEach(i -> {
+                atomicInteger.getAndIncrement();
+            });
+        });
+
+        thread03.start();
+        thread04.start();
+        thread03.join();
+        thread04.join();
+
+        System.out.println(atomicInteger.get());
+    }
+}
---
+++ big-fool/big-fool-practice/src/main/java/top/lconcise/practice/VolatileDemo03.java
@@ -0,0 +1,33 @@
+package top.lconcise.practice;
+
+/**
+ * @author liusj
+ * @date 2020/11/27
+ */
+public class VolatileDemo03 {
+
+    /**
+     * 虽然加了同步锁，但是多线程下并不是线程安全的。
+     *
+     * instance = new VolatileDemo03()在实际执行的时候会被拆分为以下三个步骤:
+     * 1. 分配存储VolatileDemo03对象的内存空间
+     * 2. 将instance指向刚刚分配的内存空间
+     * 3. 初始化VolatileDemo03对象
+     */
+
+    // 私有化构造方法
+    private VolatileDemo03() {
+    }
+
+    // 单例对象
+    private static VolatileDemo03 instance = null;
+
+    public static VolatileDemo03 getInstance() {
+        if (instance == null) { // 双检索
+            synchronized (VolatileDemo03.class) { // 同步锁
+                instance = new VolatileDemo03();
+            }
+        }
+        return instance;
+    }
+}
