5d119dcfe8d78ea62aa6685f1e9560f0935ecdd1
liusj <605715694>
2020-12-11 18:33:12 +0800

    feat(practice):HashMap(JDK7) 死链.

---
+++ big-fool/big-fool-practice/src/main/java/top/lconcise/practice/HashMashEndlessLoop.java
@@ -0,0 +1,30 @@
+package top.lconcise.practice;
+
+import java.util.HashMap;
+
+/**
+ * HashMap(JDK7) 死链.
+ * <p>
+ * 启动10万个线程，以System.nanoTime()返回的Long值为Key,自定义对象EasyCoding为Value,运行环境是JDK7。
+ *
+ * @author liusj
+ * @date 2020/12/11
+ */
+public class HashMashEndlessLoop {
+
+    private static HashMap<Long, EasyCoding> map = new HashMap<>();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            (new Thread() {
+                @Override
+                public void run() {
+                    map.put(System.nanoTime(), new EasyCoding());
+                }
+            }).start();
+        }
+    }
+}
+
+class EasyCoding {
+}
